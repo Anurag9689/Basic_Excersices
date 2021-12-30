b1f1aed8002ce290b094e1172f3aa9f215baf722
Anurag Pandey <anuragpandey9689@gmail.com>
2021-12-30 21:08:12 +0530

    Excersice_1_question_4

--- Excersice_1/Q4_Reinforcement_Ex.py
+++ Excersice_1/Q4_Reinforcement_Ex.py
@@ -2,14 +2,18 @@
 # Write a short Python function that takes a positive integer n and returns
 # the sum of the squares of all the positive integers smaller than n.
 
-def sum_of_squares(n):  # Defining a function which will make a list of squares of natural numbers less than argument
-    # number 'n' & Add them all together
-    num_list = [k*k for k in range(1, n)]  # making a list of squares of natural number less than 'n'
-    print(num_list)   # Printing the list
-    total_sum = 0   # declaring & initiating a variable which will store total sum
-    for each_number in num_list:  # loop which will sum each element one by one
-        total_sum += each_number  # expression to sum the numbers
-    return total_sum  # Returning the total_sum
+def sum_of_squares(n):
+    return int((n*(n+1)*(2*n+1))/6 - n*n)
+
+# Other program
+# def sum_of_squares(n):  # Defining a function which will make a list of squares of natural numbers less than argument
+#     # number 'n' & Add them all together
+#     num_list = [k*k for k in range(1, n)]  # making a list of squares of natural number less than 'n'
+#     print(num_list)   # Printing the list
+#     total_sum = 0   # declaring & initiating a variable which will store total sum
+#     for each_number in num_list:  # loop which will sum each element one by one
+#         total_sum += each_number  # expression to sum the numbers
+#     return total_sum  # Returning the total_sum
 
 
 print(sum_of_squares(11))
